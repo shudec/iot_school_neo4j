023eeee6c13ecaade12a91a2cc75929486ced6f7
HUDEC <stephane.hudec@atos.net>
2020-11-10 09:19:28 +0100

    solution Ex2

---
+++ Ex2.py
@@ -0,0 +1,38 @@
+from InitDB import session
+
+# delete all nodes
+session.run("MATCH ()-[r]-() DELETE r")
+session.run("MATCH (n) DELETE n")
+
+result = session.run("CREATE (ee:Person { name: \"Emil\", from: \"Sweden\", klout: 99 })  RETURN id(ee) AS node_id")
+emil = result.single()
+print("emil id: {}".format(emil["node_id"]))
+
+
+result = session.run("MATCH (ee:Person) WHERE ee.name = \"Emil\" RETURN ee;")
+ee = result.single()
+print("\nemil : {}".format(ee.value()))
+
+req = """MATCH (ee:Person) WHERE ee.name = "Emil"
+CREATE (js:Person { name: "Johan", from: "Sweden", learn: "surfing" }),
+(ir:Person { name: "Ian", from: "England", title: "author" }),
+(rvb:Person { name: "Rik", from: "Belgium", pet: "Orval" }),
+(ally:Person { name: "Allison", from: "California", hobby: "surfing" }),
+(ee)-[:KNOWS {since: 2001}]->(js),(ee)-[:KNOWS {rating: 5}]->(ir),
+(js)-[:KNOWS]->(ir),(js)-[:KNOWS]->(rvb),
+(ir)-[:KNOWS]->(js),(ir)-[:KNOWS]->(ally),
+(rvb)-[:KNOWS]->(ally)"""
+result = session.run(req)
+
+req = """MATCH (ee:Person)-[:KNOWS]-(friends)
+WHERE ee.name = "Emil" RETURN ee, friends"""
+result = session.run(req)
+print("\nfriends of {} : ".format(result.peek()["ee"]))
+for r in result:
+    print("  - {}".format(r["friends"]))
+
+req= """MATCH (js:Person)-[:KNOWS]-()-[:KNOWS]-(surfer)
+WHERE js.name = \"Johan\" AND surfer.hobby = \"surfing\"
+RETURN DISTINCT surfer"""
+result = session.run(req)
+print("\nsurfer : {}".format(result.peek()["surfer"]))
